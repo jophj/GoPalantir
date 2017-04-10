3349ddc8fb7890c3c543f7f361382b002ac12d9a
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-04-10 19:18:04 +0200

    Added GymPokemon model

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dbHost = '192.168.1.100'
+const dbName = 'goPalantirDev'
+mongoose.connect(`mongodb://${dbHost}/${dbName}`);
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log(`Connected to database at mongodb://${dbHost}/${dbName}`)
+});
 
 const app = express()
 
-const hostname = '0.0.0.0';
-const port = 3009;
+const hostname = '0.0.0.0'
+const port = 3009
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.post('/', function (req, res) {
   const message = req.body;
-  if (message.type == 'gym_details')
+  if (message.type == 'gym_details') {
     console.log(message)
-    
+  }
   res.end()
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`GoPalantir webhook running at http://${hostname}:${port}/`)
+})
