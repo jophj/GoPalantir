398a23c72f4485c75d863aaa537b53049edcb37c
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-04-12 10:48:00 +0200

    Gym middlewares saves data

--- gym-details-middleware.js
+++ gym-details-middleware.js
@@ -6,7 +6,7 @@ const gymDetailsMiddleware = function (req, res, next) {
   if (message.type == 'gym_details') {
     let gymUpdate = new GymDetails(message.message)
     let gymPokemons = gymUpdate.pokemon.map( p => new GymPokemon(p))
-    gymUpdate.save((err) => console.log(err || 'Gym details update saved'))
+    gymUpdate.save((err) => console.log(err || 'Gym details update saved ' + gymUpdate.id))
   }
 
   next()
--- gym-middleware.js
+++ gym-middleware.js
@@ -4,7 +4,7 @@ const gymMiddleware = function (req, res, next) {
   const message = req.body;
   if (message.type == 'gym') {
     let gymUpdate = new Gym(message.message)
-    gymUpdate.save((err) => console.log(err || 'Gym update saved'));
+    gymUpdate.save((err) => console.log(err || 'Gym update saved ' + gymUpdate.gym_id));
   }
 
   next()
