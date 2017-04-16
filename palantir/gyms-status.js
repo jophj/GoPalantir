7a98a643407bea1f4549742c035ad89021cfee49
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-04-16 16:06:10 +0200

    Added gym status module

---
+++ palantir/gyms-status.js
@@ -0,0 +1,51 @@
+const Promise = require('bluebird')
+const Gym = require('../schemas/gym').model
+const GymDetails = require('../schemas/gym-details').model
+
+function queryGymsStatus() {
+  let promise = new Promise((resolve, reject) => {
+    Gym.aggregate([
+      { $sort: { date: -1 } },
+      { $group: { "_id": "$gym_id", gym: { $first: "$$ROOT" }, activity: { $sum: 1 } } },
+      { $sort: { activity: -1 } },
+    ]).exec((err, gyms) => {
+      if (!err) resolve(gyms)
+      else reject(err)
+    })
+  })
+  return promise
+}
+
+function queryGymsDetails() {
+  let promise = new Promise((resolve, reject) => {
+    GymDetails.aggregate([
+      { $sort: { date: -1 } },
+      { $group: { _id: "$id", details: { $first: "$$ROOT" } } },
+    ]).exec((err, details) => {
+      if (!err) resolve(details)
+      else reject(err)
+    })
+  })
+  return promise
+}
+
+function getGymsStatus() {
+  let gymsStatus = null;
+
+  return queryGymsStatus()
+  .then(gymsStatusResult => {
+    gymsStatus = gymsStatusResult
+    return queryGymsDetails()
+  })
+  .then((gymDetailsResult) => {
+    let gymDetailsKeyValue = {}
+    gymDetailsResult.forEach(function(gymDetails) {
+      gymDetailsKeyValue[gymDetails.details.id] = gymDetails
+    });
+
+    gymsStatus.forEach(g => g.details = gymDetailsKeyValue[g.gym.gym_id])
+    return gymsStatus
+  })
+}
+
+module.exports = getGymsStatus
